97badc3ba9f26cd64a536cd5604588a7c32f3a6c
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 11:25:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,50 @@
-const API=window.SpeechRecognition||window.webkitSpeechRecognition
+const API = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-if (API){
-    count=0;
-    const recognition=new API()
-    recognition.continouse=true;
-    recognition.lang="ko-kr"
-    const button=document.querySelector('.speech-recognition')
-    const speechResult=document.querySelector('.result')
-    const photo1 = document.querySelector("#photo1");
+if (API) {
+  let count = 0;
+  const recognition = new API();
+  recognition.continuous = true;
+  recognition.lang = "ko-kr";
+  const button = document.querySelector(".speech-recognition");
+  const speechResult = document.querySelector(".result");
+  const photo1 = document.querySelector("#photo1");
 
-    const imagePaths=[
-        '세탁기.jpg',
-    ]
+  const imagePaths = ["세탁기.jpg"];
 
-    button.addEventListener('click',()=>{
-        recognition.start();
-        button.textContent='Listening...'
-    })
+  button.addEventListener("click", () => {
+    recognition.start();
+    button.textContent = "Listening...";
+  });
 
-    recognition.onresult=(event)=>{
-        for(const result of event.results){
-            speechResult.textContent=result[0].transcript
+  recognition.onresult = (event) => {
+    for (const result of event.results) {
+      const transcript = result[0].transcript;
+      speechResult.textContent = transcript;
 
-            if(transcript.includes('감자'))
-            {
-                changephoto()
-                count++;
-            }else if (transcript.includes('고구마'))
-            {
-                changephoto()
-                count++;
-            }
-        }
+      if (transcript.includes("감자")) {
+        changephoto();
+        count++;
+      } else if (transcript.includes("고구마")) {
+        changephoto();
+        count++;
+      }
     }
-    function changephoto()
-    {
-        if (count==5) {
-            console.log("게임 끝.");
-        }
+  };
+
+  function changephoto() {
+    if (count == 5) {
+      console.log("게임 끝.");
+      return;
     }
+
     const randomIndex = Math.floor(Math.random() * imagePaths.length);
     const randomImagePath = imagePaths[randomIndex];
     photo1.src = randomImagePath;
     imagePaths.splice(randomIndex, 1);
+  }
+
+  const randomIndex = Math.floor(Math.random() * imagePaths.length);
+  const randomImagePath = imagePaths[randomIndex];
+  photo1.src = randomImagePath;
+  imagePaths.splice(randomIndex, 1);
 }
