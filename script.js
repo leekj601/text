27475dfec6eb4fd232f4ad2928e039392482d2c7
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 14:04:54 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const API = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (API) {
-  const imagePaths = ['감자.jpg','고구마.jpg','고양이.jpg', '강아지.jpg', '세탁기.jpg','밤.jpg'];
+  const imagePaths = ['감자.jpg', '고구마.jpg', '고양이.jpg', '강아지.jpg', '세탁기.jpg','밤.jpg'];
   const recognition = new API();
 
   recognition.continuous = true;
@@ -9,6 +9,10 @@ if (API) {
 
   const button = document.querySelector('.speech-recognition');
   const speechResult = document.querySelector('.result');
+  const photo1 = document.querySelector('#photo1');
+  const photo2 = document.querySelector('#photo2');
+  let photo1Name = extractImageName(photo1.src);
+  let photo2Name = extractImageName(photo2.src);
 
   button.addEventListener('click', () => {
     recognition.start();
@@ -22,23 +26,39 @@ if (API) {
       const transcript = results[i][0].transcript;
       speechResult.textContent = transcript;
 
-      if (transcript.includes(photo1.src.split('/').pop().split('.')[0])) {
+      if (transcript.includes(photo1Name)) {
         changephoto1();
       }
-      if (transcript.includes(photo2.src.split('/').pop().split('.')[0])) {
+      if (transcript.includes(photo2Name)) {
         changephoto2();
       }
     }
   };
 
-  function changephoto1() {
-    const photo1 = document.querySelector('#photo1');
-    const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
-    photo1.src = image;
+  function extractImageName(imagePath) {
+    const imageName = imagePath.split('/').pop().split('.')[0];
+    return imageName;
   }
+
+  function changephoto1() {
+    const newPhotoIndex = getRandomPhotoIndex(photo1Name);
+    const newPhotoName = extractImageName(imagePaths[newPhotoIndex]);
+    photo1.src = imagePaths[newPhotoIndex];
+    photo1Name = newPhotoName;
+  }
+
   function changephoto2() {
-    const photo2 = document.querySelector('#photo2');
-    const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
-    photo2.src = image;
+    const newPhotoIndex = getRandomPhotoIndex(photo2Name);
+    const newPhotoName = extractImageName(imagePaths[newPhotoIndex]);
+    photo2.src = imagePaths[newPhotoIndex];
+    photo2Name = newPhotoName;
+  }
+
+  function getRandomPhotoIndex(excludeName) {
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * imagePaths.length);
+    } while (extractImageName(imagePaths[randomIndex]) === excludeName);
+    return randomIndex;
   }
 }
