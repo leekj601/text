1bbb7ae8374667046303d73d6607298218eadb69
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 13:47:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (API) {
   const imagePaths = ['감자.jpg', '고구마.jpg', '고양이.jpg', '강아지.jpg', '세탁기.jpg'];
+  const imageNames = ['감자', '고구마', '고양이', '강아지', '세탁기'];
   const recognition = new API();
 
   recognition.continuous = true;
@@ -23,9 +24,10 @@ if (API) {
       speechResult.textContent = transcript;
 
       for (let j = 0; j < imagePaths.length; j++) {
-        const imageName = imagePaths[j].split('.')[0];
+        const imageName = imageNames[j];
         if (transcript.includes(imageName)) {
           changephoto(j);
+          speechResult.textContent = imageName;
           break;
         }
       }
@@ -35,20 +37,17 @@ if (API) {
   function changephoto(photoNumber) {
     const photo1 = document.querySelector('#photo1');
     const photo2 = document.querySelector('#photo2');
-    
+
+    let newImageIndex;
+    do {
+      newImageIndex = Math.floor(Math.random() * imagePaths.length);
+    } while (newImageIndex === photoNumber);
+
     if (photoNumber === 0) {
-      let randomIndex = Math.floor(Math.random() * imagePaths.length);
-      while (randomIndex === 0) {
-        randomIndex = Math.floor(Math.random() * imagePaths.length);
-      }
-      const image = imagePaths[randomIndex];
+      const image = imagePaths[newImageIndex];
       photo1.src = image;
     } else if (photoNumber === 1) {
-      let randomIndex = Math.floor(Math.random() * imagePaths.length);
-      while (randomIndex === 1) {
-        randomIndex = Math.floor(Math.random() * imagePaths.length);
-      }
-      const image = imagePaths[randomIndex];
+      const image = imagePaths[newImageIndex];
       photo2.src = image;
     }
   }
