0735deeb1e3ec42c23e8c0d2b85270b494a9bf6a
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 12:50:15 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 const API = window.SpeechRecognition || window.webkitSpeechRecognition
+
 if (API) {
-    let count = 0;
-    const recognition = new API();
-    recognition.continuous = true;
-    recognition.lang = "ko-kr";
-    const button = document.querySelector(".speech-recognition");
-    const speechResult = document.querySelector(".result");
-    const photo1 = document.querySelector("#photo1");
-    const photo2 = document.querySelector("#photo2");
-  
-    const imagePaths = ["세탁기.jpg", "고양이.jpg", "강아지.jpg"];
-  
-    button.addEventListener("click", () => {
-      recognition.start();
-      button.textContent = "Listening...";
-    })
-  
-    recognition.onresult = (event) => {
-      let transcript = "";
-      for (const result of event.results) {
-        transcript = result[0].transcript;
-        speechResult.textContent = transcript;
-      }
-      if (transcript.includes("감자")) {
-        changephoto1();
-      }
+   const recognition = new API()
+
+   recognition.continouse = true;
+   recognition.lang = 'ko-kr'
+
+   const button = document.querySelector('.speech-recognition')
+   const speechResult = document.querySelector('.result')
+
+   button.addEventListener('click', () => {
+    recognition.start();
+    button.textContent = 'Listening...'
+   })
+
+
+   recognition.onresult = (event) => {
+    for(const result of event.results) {
+        speechResult.textContent = result[0].transcript
+        if (transcript.includes("감자")) {
+            changephoto1();
+          }
+          
     }
-  
-    function changephoto1() {
-      const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
-      photo1.src = `src/${image}`;
-    }
+   }
+     
+   function changephoto1() {
+    const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+    photo1.src = `src/${image}`;
   }
-  
+
+}
