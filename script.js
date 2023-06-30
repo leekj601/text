01ac3d67c087eb79dc69b878637c4cfc9bf6b487
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 11:04:48 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const API=window.SpeechRecognition||window.webkitSpeechRecognition
+
+if (API){
+    const recognition=new API()
+    recognition.continouse=true;
+    recognition.lang="en-GB"
+    const button=document.querySelector('.speech-recognition')
+    const speechResult=document.querySelector('.result')
+
+    button.addEventListener('click',()=>{
+        recognition.start();
+        button.textContent='Listening...'
+    })
+
+    recognition.onresult=(event)=>{
+        for(const result of event.results){
+            speechResult.textContent=result[0].transcript
+        }
+    }
+}
