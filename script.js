63af45cd4c63958f5cc0d9bdedbfb847cfff0c05
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 14:01:14 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-recognition.onresult = (event) => {
+const API = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (API) {
+  const imagePaths = ['감자.jpg','고구마.jpg','고양이.jpg', '강아지.jpg', '세탁기.jpg','밤.jpg'];
+  const recognition = new API();
+
+  recognition.continuous = true;
+  recognition.lang = 'ko-kr';
+
+  const button = document.querySelector('.speech-recognition');
+  const speechResult = document.querySelector('.result');
+
+  button.addEventListener('click', () => {
+    recognition.start();
+    button.textContent = 'Listening...';
+  });
+
+  recognition.onresult = (event) => {
     const results = event.results;
-  
+
     for (let i = 0; i < results.length; i++) {
       const transcript = results[i][0].transcript;
       speechResult.textContent = transcript;
-  
-      if (imagePaths.some(photo => transcript.includes(photo.split('.')[0]))) {
-        const photoIndex = imagePaths.findIndex(photo => transcript.includes(photo.split('.')[0]));
-        if (photoIndex !== -1) {
-          changephoto(photoIndex);
-        }
+
+      if (transcript.includes(photo1.src.split('/').pop().split('.')[0])) {
+        changephoto1();
+      }
+      if (transcript.includes(photo2.src.split('/').pop().split('.')[0])) {
+        changephoto2();
       }
     }
   };
-  
-  function changephoto(photoIndex) {
+
+  function changephoto1() {
     const photo1 = document.querySelector('#photo1');
-    const photo2 = document.querySelector('#photo2');
-  
-    let newImageIndex;
-    do {
-      newImageIndex = Math.floor(Math.random() * imagePaths.length);
-    } while (newImageIndex === photoIndex);
-  
-    if (photoIndex === 0) {
-      const image = imagePaths[newImageIndex];
-      photo1.src = image;
-    } else if (photoIndex === 1) {
-      const image = imagePaths[newImageIndex];
-      photo2.src = image;
-    }
+    const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+    photo1.src = image;
   }
-  
+  function changephoto2() {
+    const photo2 = document.querySelector('#photo2');
+    const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+    photo2.src = image;
+  }
+}
