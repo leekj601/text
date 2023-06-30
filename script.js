8c67477da8d3d38b61281673ff956d671519ca14
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 11:08:40 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const API=window.SpeechRecognition||window.webkitSpeechRecognition
 
 if (API){
+    count=0;
     const recognition=new API()
     recognition.continouse=true;
-    recognition.lang="en-GB"
+    recognition.lang="ko-GB"
     const button=document.querySelector('.speech-recognition')
     const speechResult=document.querySelector('.result')
+
+    const imagePaths=[
+
+    ]
 
     button.addEventListener('click',()=>{
         recognition.start();
@@ -15,6 +20,22 @@ if (API){
     recognition.onresult=(event)=>{
         for(const result of event.results){
             speechResult.textContent=result[0].transcript
+
+            if(transcript.includes('potato'))
+            {
+                changephoto()
+                count++;
+            }else if (transcript.includes('sweet potato'))
+            {
+                changephoto()
+                count++;
+            }
+        }
+    }
+    function changephoto()
+    {
+        if (count==5) {
+            console.log("게임 끝.");
         }
     }
 }
