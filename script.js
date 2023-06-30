dfba1968809535572c3fcfc9c5a9cd08d3049db6
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-06-30 13:17:10 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-const API = window.SpeechRecognition || window.webkitSpeechRecognition
+const API = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 if (API) {
-    const imagePaths = ['고양이.jpg', '강아지.jpg', '세탁기.jpg'];
-   const recognition = new API()
+  const imagePaths = ['고양이.jpg', '강아지.jpg', '세탁기.jpg'];
+  const recognition = new API();
 
-   recognition.continouse = true;
-   recognition.lang = 'ko-kr'
+  recognition.continuous = true;
+  recognition.lang = 'ko-kr';
 
-   const button = document.querySelector('.speech-recognition')
-   const speechResult = document.querySelector('.result')
+  const button = document.querySelector('.speech-recognition');
+  const speechResult = document.querySelector('.result');
 
-   button.addEventListener('click', () => {
+  button.addEventListener('click', () => {
     recognition.start();
-    button.textContent = 'Listening...'
-   })
+    button.textContent = 'Listening...';
+  });
 
+  recognition.onresult = (event) => {
+    const results = event.results;
 
-   recognition.onresult = (event) => {
-    for(const result of event.results) {
-        speechResult.textContent = result[0].transcript
-        if (result.includes("감자")) {
-            changephoto1();
-          }
+    for (let i = 0; i < results.length; i++) {
+      const transcript = results[i][0].transcript;
+      speechResult.textContent = transcript;
+
+      if (transcript.includes('감자')) {
+        changephoto1();
+      }
     }
-   }
-   function changephoto1() {
+  };
+
+  function changephoto1() {
     const photo1 = document.querySelector('#photo1');
     const image = imagePaths[Math.floor(Math.random() * imagePaths.length)];
     photo1.src = image;
